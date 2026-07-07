e04e7347067e891b2d60a2170a395a86e614b983
DHRUVILPATEL2303 <pateldhruvil2303@gmail.com>
2026-07-07 14:51:06 +0530

    add : register content and verify content

--- src/lib.rs
+++ src/lib.rs
@@ -1,105 +1,100 @@
-//! Stylus Hello World
-//!
-//! The following contract implements the Counter example from Foundry.
-//!
-//! ```solidity
-//! contract Counter {
-//!     uint256 public number;
-//!     function setNumber(uint256 newNumber) public {
-//!         number = newNumber;
-//!     }
-//!     function increment() public {
-//!         number++;
-//!     }
-//! }
-//! ```
-//!
-//! The program is ABI-equivalent with Solidity, which means you can call it from both Solidity and Rust.
-//! To do this, run `cargo stylus export-abi`.
-//!
-//! Note: this code is a template-only and has not been audited.
-
-// Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
 extern crate alloc;
 
-/// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*};
 
-// Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
-sol_storage! {
-    #[entrypoint]
-    pub struct Counter {
-        uint256 number;
-    }
+use alloc::string::String;
+use alloc::vec::Vec;
+use alloy_primitives::{Address, B256, ruint::aliases::U64};
+use alloy_sol_types::sol;
+use stylus_sdk::{prelude::*, storage::*, stylus_core::errors::MethodError};
+
+sol! {
+    event ContentRegistered(
+        bytes32 indexed sha256hash,
+        address indexed creator,
+        uint64 phash,
+        uint64 timestamp,
+        string ipfsCid,
+        string aitool
+    );
+    error ContentAlreadyRegistered(bytes32 sha256hash);
+    error ContentNotFound(bytes32 sha256hash);
 }
 
-/// Declare that `Counter` is a contract with the following external methods.
+#[storage]
+pub struct ContentRecord {
+    pub creator: StorageAddress,
+    pub timestamp: StorageU64,
+    pub phash: StorageU64,
+    pub ipfs_cid: StorageString,
+    pub ai_tool: StorageString,
+    pub is_registered: StorageBool,
+}
+
+#[storage]
+#[entrypoint]
+pub struct VeritraceRegistry {
+    pub registry: StorageMap<B256, ContentRecord>,
+}
+
 #[public]
-impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
+impl VeritraceRegistry {
+    pub fn register_content(
+        &mut self,
+        sha256hash: B256,
+        phash: u64,
+        ipfs_cid: String,
+        ai_tool: String,
+    ) -> Result<(), Vec<u8>> {
+        let creator = self.vm().msg_sender();
+        let timestamp = self.vm().block_timestamp();
+        let mut record = self.registry.setter(sha256hash);
+
+        if record.is_registered.get() {
+            return Err(ContentAlreadyRegistered {
+                sha256hash: sha256hash.into(),
+            }
+            .encode());
+        }
+
+
+        record.creator.set(creator);
+        record.timestamp.set(U64::from(timestamp));
+        record.phash.set(U64::from(phash));
+        record.ipfs_cid.set_str(&ipfs_cid);
+        record.ai_tool.set_str(&ai_tool);
+        record.is_registered.set(true);
+
+        self.vm().log(ContentRegistered {
+            sha256hash: sha256hash.into(),
+            creator,
+            phash,
+            timestamp,
+            ipfsCid: ipfs_cid,
+            aitool: ai_tool,
+        });
+
+        Ok(())
     }
+    pub fn verify_content(
+        &self,
+        sha256hash: B256,
+    ) -> Result<(Address, u64, u64, String, String), Vec<u8>> {
+        let record = self.registry.getter(sha256hash);
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
-    }
+        if !record.is_registered.get() {
+            return Err(ContentNotFound {
+                sha256hash: sha256hash.into(),
+            }
+            .encode());
+        }
 
-    /// Multiplies `number` by `new_number` and updates its value in storage.
-    pub fn mul_number(&mut self, new_number: U256) {
-        self.number.set(new_number * self.number.get());
-    }
-
-    /// Increments `number` by `new_number` and updates its value in storage.
-    pub fn add_number(&mut self, new_number: U256) {
-        self.number.set(new_number + self.number.get());
-    }
-
-    /// Increments `number` by 1 and updates its value in storage.
-    pub fn increment(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
-    }
-
-    /// Adds the wei value from msg_value to the number in storage.
-    #[payable]
-    pub fn add_from_msg_value(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + self.vm().msg_value());
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_counter() {
-        use stylus_sdk::testing::*;
-        let vm = TestVM::default();
-        let mut contract = Counter::from(&vm);
-
-        assert_eq!(U256::ZERO, contract.number());
-
-        contract.increment();
-        assert_eq!(U256::from(1), contract.number());
-
-        contract.add_number(U256::from(3));
-        assert_eq!(U256::from(4), contract.number());
-
-        contract.mul_number(U256::from(2));
-        assert_eq!(U256::from(8), contract.number());
-
-        contract.set_number(U256::from(100));
-        assert_eq!(U256::from(100), contract.number());
-
-        // Override the msg value for future contract method invocations.
-        vm.set_value(U256::from(2));
-
-        contract.add_from_msg_value();
-        assert_eq!(U256::from(102), contract.number());
+        Ok((
+            record.creator.get(),
+            record.timestamp.get().to::<u64>(),
+            record.phash.get().to::<u64>(),
+            record.ipfs_cid.get_string(),
+            record.ai_tool.get_string(),
+        ))
     }
 }
